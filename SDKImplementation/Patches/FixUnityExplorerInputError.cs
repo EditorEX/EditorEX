dee39a65ab5bb19471b4242f9c690c1c4752041b
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2025-02-05 21:02:39 -0500

    Add Unity Explorer fix

---
+++ SDKImplementation/Patches/FixUnityExplorerInputError.cs
@@ -0,0 +1,22 @@
+﻿using BeatmapEditor3D.InputSystem;
+using SiraUtil.Affinity;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
+using Zenject;
+
+namespace EditorEX.SDKImplementation.Patches
+{
+    public class FixUnityExplorerInputError : IAffinity
+    {
+        [AffinityPatch(typeof(InputReceiver), "IsPointerOverUI")]
+        [AffinityPrefix]
+        private bool FixInputModuleOverride()
+        {
+            if (EventSystem.current == null || EventSystem.current.currentInputModule == null)
+            {
+                return true;
+            }
+            return EventSystem.current.currentInputModule is InputSystemUIInputModule;
+        }
+    }
+}
